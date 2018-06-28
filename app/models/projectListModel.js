8bf79aea7acd6a3b73cb1861af736579d5838d05
Rachel Wright <rachel.wright@buchanan-edwards.com>
2018-06-28 07:09:52 -0400

    Broke out project model

--- app/models/projectListModel.js
+++ app/models/projectListModel.js
@@ -1,7 +1,9 @@
 define([
     "ko"
+    , "./projectModel"
 ], function (
     ko
+    , ProjectModel
 ) {
     var 
     projectListModel = function() {
@@ -40,13 +42,31 @@ define([
         self.init = function(data){
             self.projectList(
                         // use an explicitly declared model to get access to computed observables, functions, etc
-                        data.map(function(row){return new ProjectModel(row)})
+                        data.map(function(row){return new projectModel(row)})
                     )
+        }
+
+        self.addProject = function(project){
+            self.projectList().concat(project)
+
+            return self.index(self.projectList.indexOf(project))  
+        }
+
+        self.newProject = function(){
+            var project = new projectModel({projtectId: 0})
+            self.projectList().concat(project)
+
+            return self.index(self.projectList.indexOf(project))  
         }
 
         self.spInit = function(spdata){
             self.projectList(
-                spdata.map(mapSPrecord)
+                // spdata.map(mapSPrecord)
+                spdata.map(function(row){
+                    var pm = new ProjectModel()
+                    
+                    return pm.initFromSP(row)
+                })
             )
         }
 
@@ -60,11 +80,6 @@ define([
             
         }
 
-        self.modifyProject = function(data){
-            // locate the project in the list
-
-        }
-
         nextIndex = function(){
             i = self.firstRecord() + self.numberOfRecords() >  self.projectList().length ? self.firstRecord() : self.firstRecord() + self.numberOfRecords()
             self.firstRecord(i)
@@ -75,67 +90,7 @@ define([
             self.firstRecord(i)
             self.index(null)
         }
-    },
+    }
 
-    projectModel = function (project) {
-
-        var self = this;
-
-        self.projectId = project.projectId
-        self.projectName = ko.observable(project.projectName)
-        self.projectDescription = ko.observable(project.projectDescription)
-        self.businessUnit = ko.observable(project.businessUnit)
-        self.projectNumber = ko.observable(project.projectNumber)
-        self.projectManager = ko.observable(project.projectManager)
-        self.customer = ko.observable(project.customer)
-        self.customerDetail = ko.observable(project.customerDetail)
-        self.contractType = ko.observable(project.contractType)
-        self.contractValue = ko.observable(project.contractValue)
-        self.practices = ko.observableArray(project.practices)
-        self.placeOfPerformance = ko.observable(project.placeOfPerformance)
-        self.primeContractor = ko.observable(project.primeContractor)
-        self.primeContractorName = ko.observable(project.primeContractorName)
-        self.securityClearedWork = ko.observable(project.securityClearedWork)
-        self.securitySCIF = ko.observable(project.securitySCIF)
-        self.technicalLead = ko.observable(project.technicalLead)
-
-        self.createdBy = ko.observable(project.createdBy)
-        self.updatedBy = ko.observable(project.updatedBy)
-        self.created = ko.observable(project.created)
-        self.updated = ko.observable(project.updated)
-
-    },
-
-    // This function maps the SharePoint List Fields onto the knockout model. The field name should be the internal name (not display name)
-    mapSPrecord = function(sprecord){
-        var row = {}
-        
-        row.projectId = sprecord.ID
-        row.projectName = sprecord.Title
-        row.projectDescription = sprecord.CategoryDescription
-        row.businessUnit = sprecord.BusinessUnit == null ? null : sprecord.BusinessUnit.get_lookupValue()
-        row.projectNumber = sprecord.ProjectNumber
-        row.projectManager = sprecord.ProjectManager == null ? null : sprecord.ProjectManager.get_lookupValue()
-        row.technicalLead = sprecord.TechnicalLead == null ? null : sprecord.TechnicalLead.get_lookupValue()
-        row.customer = sprecord.Customer == null || sprecord.Customer[0] == null ? null : sprecord.Customer[0].get_lookupValue()
-        row.customerDetail = sprecord.CustomerDetail
-        row.contractType = sprecord.ContractType == null ? null : sprecord.ContractType.get_lookupValue()
-        row.placeOfPerformance = sprecord.PlaceofPerformance
-        row.primeContractor = sprecord.PrimeContractor
-        row.primeContractorName = sprecord.PrimeContractorName
-        row.securityClearedWork = sprecord.SecurityClearedWork
-        row.securitySCIF = sprecord.SecuritySCIF
-        row.contractValue = sprecord.ContractValue
-        row.practices = sprecord.Practices != null && sprecord.Practices.count > 0 ? sprecord.Practices.map(function(p){return p.get_lookupValue()}) : null
-
-        row.createdBy = sprecord.Author == null ? null : sprecord.Author.get_lookupValue()
-        row.updatedBy = sprecord.Editor == null ? null : sprecord.Editor.get_lookupValue()
-        row.created = sprecord.Created == null ? null : sprecord.Created
-        row.updated = sprecord.Modified == null ? null : sprecord.Modified
-
-        return new projectModel(row)
-    
-    };
-
-    return projectListModel;
+    return projectListModel
 });
---
+++ app/models/projectModel.js
@@ -0,0 +1,95 @@
+define([
+    "ko"
+], function (
+    ko
+) {
+    var  
+
+    projectModel = function () {
+        var self = this;
+    
+        self.isDirty = ko.observable(false)
+        self.errors = ko.observableArray()
+
+        self.projectId = ko.observable()
+        self.projectName = ko.observable()
+        self.projectDescription = ko.observable()
+        self.businessUnit = ko.observable()
+        self.projectNumber = ko.observable()
+        self.projectManager = ko.observable()
+        self.customer = ko.observable()
+        self.customerDetail = ko.observable()
+        self.contractType = ko.observable()
+        self.contractValue = ko.observable()
+        self.practices = ko.observableArray()
+        self.placeOfPerformance = ko.observable()
+        self.primeContractor = ko.observable()
+        self.primeContractorName = ko.observable()
+        self.securityClearedWork = ko.observable()
+        self.securitySCIF = ko.observable()
+        self.technicalLead = ko.observable()
+
+        self.createdBy = ko.observable()
+        self.updatedBy = ko.observable()
+        self.created = ko.observable()
+        self.updated = ko.observable()
+
+        self.init = function(project){
+            self.projectId(project.projectId)
+            self.projectName(project.projectName)
+            self.projectDescription(project.projectDescription)
+            self.businessUnit(project.businessUnit)
+            self.projectNumber(project.projectNumber)
+            self.projectManager(project.projectManager)
+            self.customer(project.customer)
+            self.customerDetail(project.customerDetail)
+            self.contractType(project.contractType)
+            self.contractValue(project.contractValue)
+            self.practicesArray(project.practices)
+            self.placeOfPerformance(project.placeOfPerformance)
+            self.primeContractor(project.primeContractor)
+            self.primeContractorName(project.primeContractorName)
+            self.securityClearedWork(project.securityClearedWork)
+            self.securitySCIF(project.securitySCIF)
+            self.technicalLead(project.technicalLead)
+    
+            self.createdBy(project.createdBy)
+            self.updatedBy(project.updatedBy)
+            self.created(project.created)
+            self.updated(project.updated)
+        }
+
+        // This function maps the SharePoint List Fields onto the knockout model. The field name should be the internal name (not display name)
+        self.initFromSP = function(sprecord){
+
+            self.projectId(sprecord.ID)
+            self.projectName(sprecord.Title)
+            self.projectDescription(sprecord.CategoryDescription)
+            self.businessUnit(sprecord.BusinessUnit == null ? null : sprecord.BusinessUnit.get_lookupValue())
+            self.projectNumber(sprecord.ProjectNumber)
+            self.projectManager(sprecord.ProjectManager == null ? null : sprecord.ProjectManager.get_lookupValue())
+            self.technicalLead(sprecord.TechnicalLead == null ? null : sprecord.TechnicalLead.get_lookupValue())
+            self.customer(sprecord.Customer == null || sprecord.Customer[0] == null ? null : sprecord.Customer[0].get_lookupValue())
+            self.customerDetail(sprecord.CustomerDetail)
+            self.contractType(sprecord.ContractType == null ? null : sprecord.ContractType.get_lookupValue())
+            self.placeOfPerformance(sprecord.PlaceofPerformance)
+            self.primeContractor(sprecord.PrimeContractor)
+            self.primeContractorName(sprecord.PrimeContractorName)
+            self.securityClearedWork(sprecord.SecurityClearedWork)
+            self.securitySCIF(sprecord.SecuritySCIF)
+            self.contractValue(sprecord.ContractValue)
+            self.practices(sprecord.Practices.map(function(p){return p.get_lookupValue()}))
+    
+            self.createdBy(sprecord.Author == null ? null : sprecord.Author.get_lookupValue())
+            self.updatedBy(sprecord.Editor == null ? null : sprecord.Editor.get_lookupValue())
+            self.created(sprecord.Created == null ? null : sprecord.Created)
+            self.updated(sprecord.Modified == null ? null : sprecord.Modified)
+            return self
+        }
+
+    }
+
+
+
+    return projectModel
+})
